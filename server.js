5bc1bc86c8c276c3335bfcdcea171b43fd3b2ddc
baejeongwan <jayden.bae@outlook.kr>
2022-12-13 19:46:35 +0900

    use port 3001 by default

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http')
 const app = express()
 const server = http.createServer(app)
 
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3001
 
 app.use(express.static("public"))
 

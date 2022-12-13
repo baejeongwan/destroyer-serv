fd48561ed339f0559575c1b94053d5f500faa4ba
baejeongwan <jayden.bae@outlook.kr>
2022-12-13 19:52:33 +0900

    well shall this work?

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const http = require('http')
 
 const app = express()
-const server = http.createServer(app)
 
 const port = process.env.PORT || 3001
 
 app.use(express.static("public"))
 
-
-server.listen(port, () => {
-    console.log("Server now listening @ ", port)
+app.listen(port, () => {
+    console.log("LISTENING ON PORT ", port)
 })
